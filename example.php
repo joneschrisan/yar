c49e7e1165cbdde30b211adb87cd9a324c5f186f
Chris 'CJ' Jones <joneschrisan@users.noreply.github.com>
2016-05-12 10:19:51 +0100

    Updated example to include new add_path method in yar_autoloader

--- example.php
+++ example.php
@@ -1,13 +1,22 @@
 <?php
-	set_include_path(get_include_path() . PATH_SEPARATOR . __DIR__ . DIRECTORY_SEPARATOR . "controllers");
-
+	// Include the YAR autoloader.
 	require_once(__SYSTEM__ . DIRECTORY_SEPARATOR . "yar" . DIRECTORY_SEPARATOR . "yar" . DIRECTORY_SEPARATOR . "autoloader.php");
+	
+	// Load the YAR autoloader.
 	yar_autoloader::register();
 	
+	// Add the controllers directorary to the include path.
+	yar_autoloader::add_path(__DIR__ . DIRECTORY_SEPARATOR . "controllers");
+	
+	// Create a new YAR object.
 	$router = new \yar\yar(true, null, true);
+	
+	// Add routes from a file.
 	$router->add_routes_from_file(__SYSTEM__ . DIRECTORY_SEPARATOR . "routes.json");
 	
+	// Find the route to serve.
 	$router->find_route("/home");
 	
+	// Render the page.
 	$router->render();
 ?>
